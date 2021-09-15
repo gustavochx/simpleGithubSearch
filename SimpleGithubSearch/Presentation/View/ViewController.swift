bc0515f6b485ede09015be3703c13a0cc47537b2
gustavo.soares <gustavo.soares@cabal.com.br>
2021-09-15 14:33:36 -0300

    Develop established

---
+++ SimpleGithubSearch/Presentation/View/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SimpleGithubSearch
+//
+//  Created by Gustavo Henrique Frota Soares on 15/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
